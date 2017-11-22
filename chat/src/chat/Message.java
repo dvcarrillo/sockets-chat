5b6816611f15330c7e96a46439ae82e6412d36f8
David Vargas <dv.carrillo@yahoo.com>
2017-11-22 01:23:16 +0100

    structure created

---
+++ chat/src/chat/Message.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat;
+
+/**
+ *
+ * @author dvcarrillo
+ */
+
+/**
+ * Clase Message
+ * Almacena informacion sobre un mensaje:
+ *  - ID del usuario que envia el mensaje
+ *  - Nombre del usuario
+ *  - Contenido del mensaje
+ *  - Pensar que mas hace falta
+ * 
+ *  - SETS para todos los atributos
+ *  - Idea: se puede asignar un color a cada usuario, de modo que los mensajes
+ * de cada usuario aparezcan de un color diferente en la vista
+ * 
+ * @author dvcarrillo
+ */
+
+public class Message {
+    
+}
---
+++ chat/src/chat/Server.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat;
+
+/**
+ *
+ * @author dvcarrillo
+ */
+
+/**
+ * Clase Server
+ * Almacena inforamcion sobre el servidor:
+ *  - ArrayList de objetos Mensaje: cada mensaje que le envian los clientes (private)
+ * 
+ *  - Metodo para recibir el objeto Mensaje de un servidor
+ *  - Averiguar como saber si hay mensajes nuevos (venga, es facil)
+ *  - Hacer un metodo que devuelva si hay mensajes nuevos
+ *  - Metodo para devolver el ultimo objeto Mensaje
+ *  - Lo que se os ocurra
+ * 
+ *  - RECORDAD QUE EL SERVIDOR DEBE EMPLEAR THREADS COMO EN EL EJERCICIO 
+ *  ANTERIOR, PARA ATENDER A TODOS LOS CLIENTES DE FORMA SIMULTANEA
+ * @author dvcarrillo
+ */
+
+public class Server {
+    public static void main(String [] args) {
+        
+    }
+}
